817ff8b3bee6df4c2753b17a778d41726394d5ae
radek <radekwrooobel02@gmail.com>
2024-04-26 10:51:16 +0200

    Dodany kod tworzący ten sam trójkąt, ale powstający od prawej krawędzi

--- Trojkat.cpp
+++ Trojkat.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-    //Kod tworzący trójkąt prostokątny
-    int rozmiarTrojkata = 0;
-    cout << "Podaj wymiary trojkata: ";
-    cin >> rozmiarTrojkata;
+    //Kod tworzący trójkąt prostokątny od lewej strony
+    int rozmiarTrojkataL = 0;
+    cout << "Podaj wymiary trojkata powstajacego od lewej krawedzi: ";
+    cin >> rozmiarTrojkataL;
 
-    for (int i = 0; i < rozmiarTrojkata; i++)
+    for (int i = 0; i < rozmiarTrojkataL; i++)
     {
-        for (int j = 0; j < rozmiarTrojkata; j++)
+        for (int j = 0; j < rozmiarTrojkataL; j++)
         {
             if (j <= i)
             {
@@ -24,6 +24,28 @@ int main()
         }
         cout << endl;
     }
+
+    //Trójkąt prostokątny, tym razem powstający od prawej strony
+    int rozmiarTrojkataP = 0;
+    cout << "Podaj rozmiar trojkata powstajacego od prawej krawedzi: \n";
+    cin >> rozmiarTrojkataP;
+
+    for (int i = 0; i < rozmiarTrojkataP; i++)
+    {
+        for (int j = rozmiarTrojkataP - 1; j >=0; j--)
+        {
+            if (j<=i)
+            {
+                cout << ("*");
+            }
+            else
+            {
+                cout << (" ");
+            }
+        }
+        cout << endl;
+    }
+
 
     return 0;
 }
